e4309c8ca94e53b18ea8e4e6c3518146f3ddad2b
omojolasamson <sammy_omojola@yahoo.com>
2019-12-25 21:49:59 +0100

    adding firebase to web app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,22 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCaeMkMEJgkNPF192TAaXg0QRNpSXFkcok",
+  authDomain: "tasklist-757b8.firebaseapp.com",
+  databaseURL: "https://tasklist-757b8.firebaseio.com",
+  projectId: "tasklist-757b8",
+  storageBucket: "tasklist-757b8.appspot.com",
+  messagingSenderId: "804525283353",
+  appId: "1:804525283353:web:9bc4c678cdfbde37d2b288",
+  measurementId: "G-7SXC61VV9V"
+};
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +30,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig), 
+    AngularFireAuthModule, 
+    AngularFirestoreModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  taskName: string;
+  taskList = [];
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {}
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('taskInput') input;
 
+  ionViewDidLoad(){
+    setTimeout(() => {
+        this.input.setFocus();
+    },350);
+}
+
+  addTask() {
+        if (this.taskName.length > 0) {
+            let task = this.taskName;
+            this.taskList.push(task);
+            this.taskName = "";
+            this.input.setFocus();
+        }
+    }
+
+    deleteTask(index){
+      this.taskList.splice(index, 1);
   }
+  updateTask(index) {
+    let alert = this.alertCtrl.create({
+        title: 'Update Task?',
+        message: 'Type in your new task to update.',
+        inputs: [{ name: 'editTask', placeholder: 'Task' }],
+        buttons: [{ text: 'Cancel', role: 'cancel' },
+                  { text: 'Update', handler: data => {
+                      this.taskList[index] = data.editTask; }
+                  }
+                 ]
+    });
+    alert.present();
+}
 
 }
